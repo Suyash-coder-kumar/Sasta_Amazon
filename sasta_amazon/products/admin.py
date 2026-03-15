4b9d3d05f7e1da15623282cbc56bcf8fb76b80ea
Suyash-coder-kumar <suyash1008kumar@gmail.com>
2026-03-15 12:15:44 +0530

    Fixed list_filter tuple bug

--- sasta_amazon/products/admin.py
+++ sasta_amazon/products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Product,Category
+from products.models import Product,Category
 # Register your models here.
 
 admin.site.register(Category)
@@ -7,4 +7,4 @@ admin.site.register(Category)
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display=('id','name','price','created_at')
-    list_filter=('category')
+    list_filter=('category',)
