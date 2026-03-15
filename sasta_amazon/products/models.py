57fcf1fd32450d0ced91e52cd1e7ef4fe1598452
Suyash-coder-kumar <suyash1008kumar@gmail.com>
2026-03-15 21:28:39 +0530

    feat: add product images
    Added product images using media

--- sasta_amazon/products/models.py
+++ sasta_amazon/products/models.py
@@ -28,6 +28,7 @@ class Product(BaseModel):
     category=models.ForeignKey(Category,on_delete=models.PROTECT)
     price=models.DecimalField(max_digits=10,decimal_places=2)
     description=models.TextField()
+    image=models.ImageField(upload_to="core",null=True,blank=True)
 
     class Meta:
         verbose_name_plural="Products"
