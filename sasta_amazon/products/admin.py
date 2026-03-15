9ddd8fa950bd2861792263fe71332866471ca135
Suyash-coder-kumar <suyash1008kumar@gmail.com>
2026-03-15 12:13:03 +0530

    Admin related settings
    Models registered and admin panel customized

--- sasta_amazon/products/admin.py
+++ sasta_amazon/products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from models import Product,Category
 # Register your models here.
+
+admin.site.register(Category)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('id','name','price','created_at')
+    list_filter=('category')
--- sasta_amazon/products/models.py
+++ sasta_amazon/products/models.py
@@ -12,7 +12,11 @@ class BaseModel(models.Model):
 
 class Category(BaseModel):
     name=models.CharField(max_length=200)
-    
+
+    class Meta:
+        verbose_name_plural="Categories"
+        db_table="Category table"
+
     def __str__(self):
         return self.name
 
@@ -24,4 +28,11 @@ class Product(BaseModel):
     category=models.ForeignKey(Category,on_delete=models.PROTECT)
     price=models.DecimalField(max_digits=10,decimal_places=2)
     description=models.TextField()
+
+    class Meta:
+        verbose_name_plural="Products"
+        db_table="Product table"
+
+    def __str__(self):
+        return self.name
     
