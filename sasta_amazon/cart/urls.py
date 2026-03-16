4d119b27746b2c68d6fd2fa32afcd7ea2edf43cb
Suyash-coder-kumar <suyash1008kumar@gmail.com>
2026-03-16 16:00:38 +0530

    feat: add cart page template and views
    Added cart.html and added cart increase/descrease functions

--- sasta_amazon/cart/urls.py
+++ sasta_amazon/cart/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('',views.cart,name="cart")
+    path('',views.cart,name="cart"),
+    path('add/',views.cart_add,name="cart_add"),
+    path('minus/',views.cart_minus,name="cart_minus"),
+    path('remove/',views.cart_remove,name="cart_remove"),
 ]
--- sasta_amazon/cart/views.py
+++ sasta_amazon/cart/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def cart(request):
+    return render(request,'cart.html')
+
+def cart_add(request):
+    pass
+
+def cart_minus(request):
+    pass
+
+def cart_remove(request):
     pass
