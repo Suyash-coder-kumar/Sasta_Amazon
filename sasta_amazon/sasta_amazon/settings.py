5b19f85951966a7050b1d9c27edefd91d71a1056
Suyash-coder-kumar <suyash1008kumar@gmail.com>
2026-03-16 06:19:42 +0530

    feat: start cart app
    Started cart app for model based cart and session based cart

--- sasta_amazon/sasta_amazon/settings.py
+++ sasta_amazon/sasta_amazon/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'products',
     'core',
     'shop',
+    'cart',
 ]
 
 MIDDLEWARE = [
--- sasta_amazon/sasta_amazon/urls.py
+++ sasta_amazon/sasta_amazon/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
     path('shop/',include('shop.urls')),
+    path('cart/',include('cart.urls')),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
